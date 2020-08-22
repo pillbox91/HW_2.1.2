fe2d14a60218171b8f1903ff6d479c4b91f3d865
Andrey Averyanov <averyanov1996@gmail.com>
2020-08-22 21:57:24 +0300

    1

--- HW_2.1.2/ViewController.swift
+++ HW_2.1.2/ViewController.swift
@@ -10,11 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var redColor: UIView!
+    @IBOutlet weak var yellowColor: UIView!
+    @IBOutlet weak var greenColor: UIView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redColor.layer.cornerRadius = redColor.frame.size.width/2
+        yellowColor.layer.cornerRadius = yellowColor.frame.size.width/2
+        greenColor.layer.cornerRadius = greenColor.frame.size.width/2
+        
+        redColor.alpha = 0.3
+        yellowColor.alpha = 0.3
+        greenColor.alpha = 0.3
     }
-
-
+    
+    @IBAction func startButtonpressed() {
+        if redColor.alpha == CGFloat(Float(0.3)) &&
+            yellowColor.alpha == CGFloat(Float(0.3)) &&
+            greenColor.alpha == CGFloat(Float(0.3)) {
+            redColor.alpha = 1
+            startButton.setTitle("Next", for: .normal)
+        } else if yellowColor.alpha == CGFloat(Float(0.3)) &&
+            redColor.alpha == 1 &&
+            greenColor.alpha == CGFloat(Float(0.3)) {
+            redColor.alpha = 0.3
+            yellowColor.alpha = 1
+            startButton.setTitle("Next", for: .normal)
+        } else if greenColor.alpha == CGFloat(Float(0.3)) && yellowColor.alpha == 1 && redColor.alpha == CGFloat(Float(0.3)) {
+            redColor.alpha = 0.3
+            yellowColor.alpha = 0.3
+            greenColor.alpha = 1
+            startButton.setTitle("Next", for: .normal)
+        } else {
+            redColor.alpha = 1
+            yellowColor.alpha = 0.3
+            greenColor.alpha = 0.3
+        }
+    }
+    
 }
 
